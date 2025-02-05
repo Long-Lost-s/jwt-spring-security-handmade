c0cf23b046ceb1b0a222ec6333c8fdd63203cfbf
Long-Lost-s <hoanglong2117@gmail.com>
2025-02-05 19:45:55 +0700

    build model

---
+++ src/main/java/org/example/jwtspringsecurityhandmade/JwtSpringSecurityHandmadeApplication.java
@@ -0,0 +1,13 @@
+package org.example.jwtspringsecurityhandmade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringSecurityHandmadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSpringSecurityHandmadeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/jwtspringsecurityhandmade/model/ROLENAME.java
@@ -0,0 +1,5 @@
+package org.example.jwtspringsecurityhandmade.model;
+
+public enum ROLENAME {
+    ROLE_ADMIN, ROLE_USER
+}
