1ff6cf1f49d9cc8afc53eb2f743e4e68f4aee32b
Long-Lost-s <hoanglong2117@gmail.com>
2025-02-05 21:13:08 +0700

    build JwtResponse

---
+++ src/main/java/org/example/jwtspringsecurityhandmade/configuration/jwt/JwtAuthenticationTokenFilter.java
@@ -0,0 +1,4 @@
+package org.example.jwtspringsecurityhandmade.configuration.jwt;
+
+public class JwtAuthenticationTokenFilter {
+}
---
+++ src/main/java/org/example/jwtspringsecurityhandmade/configuration/jwt/JwtResponse.java
@@ -0,0 +1,66 @@
+package org.example.jwtspringsecurityhandmade.configuration.jwt;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class JwtResponse {
+    private Long id;
+    private String token;
+    private String type = "Bearer";
+    private String username;
+    private String name;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public JwtResponse(Collection<? extends GrantedAuthority> authorities, String name, String username, String token, Long id) {
+        this.authorities = authorities;
+        this.name = name;
+        this.username = username;
+        this.token = token;
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+}
